8481e77165eef6572e69bee3013747130bc2cbe4
teemu perttula <perttula.teemu@gmail.com>
2020-08-22 17:34:12 +0300

    bootstrap imported

--- et-ui/src/index.js
+++ et-ui/src/index.js
@@ -6,6 +6,8 @@ import * as serviceWorker from './serviceWorker';
 import {createStore} from 'redux';
 import allReducer from './reducers/index'
 import {Provider} from 'react-redux'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap';
 
 const store = createStore(allReducer,
   window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
